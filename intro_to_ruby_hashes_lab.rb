6d7fed130847eea13b5cf2c4a0c091f4f705bf76
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-09 03:07:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,14 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {:name => "John Henry", :occupation => "Steel-driving man"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+  
 end
 
 def id_generator
